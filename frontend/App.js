9d6ac527f8d4681dcc9e97aaebaa952d2f4a2789
Elias Apaza <elias.apaza@sou.inteli.edu.br>
2025-06-10 10:59:03 -0300

    jogo-v1

---
+++ frontend/App.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import GameScreen from './screens/GameScreen';
+import ResultScreen from './screens/ResultScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Game" component={GameScreen} />
+        <Stack.Screen name="Result" component={ResultScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
